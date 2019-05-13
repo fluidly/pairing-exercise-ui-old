a5564df3741bac03fdf0262b11b8690c56822ccb
Michael Townsend <mike.townsend@fluidly.com>
2019-05-13 11:36:42 +0100

    🐛 Fix issue in fetch call

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from "react"
+import "./App.css"
 
 class App extends Component {
   state = {
     data: null
-  };
+  }
 
   componentDidMount = () => {
     fetch("/data")
-      .then(data => this.setState({ data }))
-      .catch(console.log);
-  };
+      .then(res => res.json())
+      .then(body => this.setState({ data: body.data }))
+      .catch(console.log)
+  }
 
   render() {
     return (
       <div className="App">
         <p>Hello world!</p>
+        <pre>
+          {this.state.data
+            ? JSON.stringify(this.state.data[0], null, "  ")
+            : null}
+        </pre>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
