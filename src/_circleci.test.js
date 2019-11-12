9cd54eb2625864b9b30d1d6f8ce57c1ae23c0db5
Paolo Escobar <paolo.escobar@fluidly.com>
2019-11-12 14:46:38 +0000

    Renames circleci test

--- src/_circleci.test.js
+++ src/_circleci.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import App from './App'
+import App from '../src/App'
 
 it('renders without crashing', () => {
   const { getByText } = render(<App />)
