abcd090c20445e10ca6d2e614163ef9767961500
Paolo Escobar <paolo.escobar@fluidly.com>
2019-11-12 15:13:43 +0000

    Test for frontend and backend

--- src/_circleci.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { render } from '@testing-library/react'
-import App from '../src/App'
-
-it('renders without crashing', () => {
-  const { getByText } = render(<App />)
-  expect(getByText('Hello world!')).toBeInTheDocument()
-})
