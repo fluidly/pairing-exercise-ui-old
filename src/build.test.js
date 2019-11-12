bd45f661a36760889912a17622944bcf5e496c6e
Paolo Escobar <paolo.escobar@fluidly.com>
2019-11-12 14:40:15 +0000

    Runs only build test

--- src/build.test.js
+++ src/build.test.js
@@ -3,6 +3,6 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 it('renders without crashing', () => {
-  render(<App />)
-  expect(1 + 1).toBe(3)
+  const { getByText } = render(<App />)
+  expect(getByText('Hello world!')).toBeInTheDocument()
 })
